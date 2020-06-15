74a4822ac4940828029fe5b80b8fec6f80b5b2a0
Your Name <you@example.com>
2020-06-15 12:38:22 -0300

    .

--- Providers/SettingPdfServiceProvider.php
+++ Providers/SettingPdfServiceProvider.php
@@ -8,6 +8,16 @@ use Illuminate\Database\Eloquent\Factory;
 class SettingPdfServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'SettingPdf';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'settingpdf';
+
+    /**
      * Boot the application events.
      *
      * @return void
@@ -37,7 +47,17 @@ class SettingPdfServiceProvider extends ServiceProvider
      */
     public function registerViews()
     {
-        $viewPath = resource_path('views/modules/settingpdf');
+                $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
+
+        $sourcePath = module_path($this->moduleName, 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        /*$viewPath = resource_path('views/modules/settingpdf');
 
         $sourcePath = __DIR__.'/../Resources/views';
 
@@ -47,7 +67,7 @@ class SettingPdfServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/settingpdf';
-        }, \Config::get('view.paths')), [$sourcePath]), 'settingpdf');
+        }, \Config::get('view.paths')), [$sourcePath]), 'settingpdf');*/
     }
 
 
@@ -60,5 +80,16 @@ class SettingPdfServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
+    }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
     }
 }
